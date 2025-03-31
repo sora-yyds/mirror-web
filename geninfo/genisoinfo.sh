c40f61520728fee12d4ca002403f2ff1394d030e
sora-yyds <2102165231@qq.com>
2025-03-31 12:05:29 +0800

    更新 genisoinfo.sh

--- geninfo/genisoinfo.sh
+++ geninfo/genisoinfo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-web=/home/sora/mirror-web
-disks="/ /home/sora/mirror-web"
+web=/www/wwwroot/mirror.s-o-r-a.eu.org/mirror-web
+disks="/ /www/wwwroot/mirror.s-o-r-a.eu.org/mirror-web"
 
 while true; do
-        python3 /home/sora/mirror-web/geninfo/z-geninfo/genisolist.py 2>/dev/null > $web/static/status/isoinfo.json
+        python3 /www/wwwroot/mirror.s-o-r-a.eu.org/mirror-web/geninfo/z-geninfo/genisolist.py 2>/dev/null > $web/static/status/isoinfo.json
         echo -n "[" > $web/static/status/disk.json
         df -B 1k --output="size,used" $disks  | awk '{if (FNR==1) ; else {if (FNR>2) printf ","; printf "{\"total_kb\":%s,\"used_kb\":%s}", $1, $2;}}' >> $web/static/status/disk.json
         echo -n "]" >> $web/static/status/disk.json
-        sleep 15s
+        sleep 6h
 done
